c8551a273b9a6a1091d68527207c801173084dd9
xiaodezhang <zhangxiaode0@gmail.com>
2018-07-02 10:41:49 +0800

    add debug=1 in make_linux_d,server listened

--- osptest/include/server.h
+++ osptest/include/server.h
@@ -1,6 +1,11 @@
 #include"osp.h"
 #include"commondemo.h"
 
+#define OSP_AGENT_SERVER_PORT 20000
+
+#define RUNNING_STATE         1
+#define IDLE_STATE            0
+
 class CSInstance : public CInstance{
 private:
         void InstanceEntry(CMessage *const);
--- osptest/source/server.cpp
+++ osptest/source/server.cpp
@@ -19,8 +19,30 @@ int main(){
                 OspQuit();
                 return -1;
         }
+        while(1)
+                OspDelay(100);
+
+        OspQuit();
         return 0;
 }
 
 void CSInstance::InstanceEntry(CMessage * const pMsg){
+
+        if(NULL == pMsg){
+                OspLog(LOG_LVL_ERROR,"[InstanceEntry] pMsg is NULL\n");
+        }
+
+        u32 curState = CurState();
+        u16 curEvent = pMsg->event;
+
+        switch(curState){
+                case IDLE_STATE:
+                        break;
+                case RUNNING_STATE:
+                        break;
+                default:
+                        break;
+        }
+
+        OspPrintf(1,0,"instance entry\n");
 }
